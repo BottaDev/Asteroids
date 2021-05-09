fb00d48aae37314e8e3af44f9881f688d34aed14
Zinovoy <lucas.zino2012@gmail.com>
2021-05-09 19:06:04 -0300

    Weapon classes code separated from IWeapon's script

---
+++ Assets/Scripts/AutomaticWeapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticWeapon : IWeapon
+{
+    PlayerInput _playerInput;
+    float fireRate;
+
+    public void Shoot()
+    {
+        var bullet = _playerInput.bulletPool.Get();
+
+        bullet.pool = _playerInput.bulletPool;
+        bullet.transform.position = _playerInput.spawnPoint.position;
+        bullet.transform.forward = _playerInput.transform.forward;
+
+        _playerInput.currentFireRate = fireRate;
+    }
+}
