c2a9f577a4efb113a41a04120094c2be304f282b
Zinovoy <lucas.zino2012@gmail.com>
2021-06-25 19:37:15 -0300

    Localization System Completed

--- Assets/Scripts/Manager/LocalizationManager.cs
+++ Assets/Scripts/Manager/LocalizationManager.cs
@@ -1,105 +1,77 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using TMPro;
 using MiniJSON;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
-public class LocalizationManager : MonoBehaviour
-{
+public class LocalizationManager : MonoBehaviour {
+
     public string rootDirectory = "/Resources/Localization";
-
     public Dictionary<SystemLanguage, Dictionary<string, string>> texts = new Dictionary<SystemLanguage, Dictionary<string, string>>();
 
-    public static LocalizationManager ins { get; private set; }
-
+    public static LocalizationManager Instance{ get; private set; }
     public static SystemLanguage language = SystemLanguage.English;
 
-    public List<TextMeshProUGUI> textsInScene = new List<TextMeshProUGUI>();
-
-    private void Awake()
-    {
-        if (ins == null)
-        {
-            ins = this;
+    void Awake(){
+        if (Instance == null) {
+            Instance = this;
+            LoadTexts();
         }
         else Destroy(this);
     }
 
-    private void Start()
-    {
+    void Start(){
+        Debug.Log("Lenguaje OS: " + Application.systemLanguage);
+        
         language = Application.systemLanguage;
-        LoadTexts();
-        SetTexts();
     }
 
-    public void SwitchLanguage()
-    {
+    public void SwitchLanguage(){
         language = language == SystemLanguage.Spanish ? SystemLanguage.English : SystemLanguage.Spanish;
-        SetTexts();
+        EventManager.Instance.Trigger("OnLanguageChange");
     }
 
-    public void LoadTexts()
-    {
+    private void LoadTexts(){
         texts = new Dictionary<SystemLanguage, Dictionary<string, string>>();
-
+        
         var allFiles = new List<string>();
-        foreach (var file in Directory.GetFiles(Application.dataPath + rootDirectory, "*.json", SearchOption.AllDirectories))
-        {
-            allFiles.Add(file);
-            //print(file + " Added");
+        foreach (var file in Directory.GetFiles(Application.dataPath + $"{rootDirectory}/", "*.json", SearchOption.AllDirectories)) {
+            var fileName = file.Substring(file.IndexOf("Localization", StringComparison.Ordinal))
+                               .Replace(@"\", @"/");
+            allFiles.Add(fileName);
         }
-
-
-        var parsedData = new Dictionary<string,string>();
-        foreach (var filePath in allFiles)
-        {
-
-            var fileName = Path.GetFileName(filePath);
-
-            string newPath = Path.GetDirectoryName(filePath);
-
-            var asset = Resources.Load<TextAsset>("Localization/" + new DirectoryInfo(newPath).Name + "/" + fileName.Replace(".json", ""));
-
-            /*
-            print("FilePath " + filePath);
-            print("Name " + fileName);
-            print("newPath " + newPath);
-            print("asset " + asset);
-            print("Localization/" + new DirectoryInfo(newPath).Name + "/" + fileName.Replace(".json", ""));
-            */
-
-            string data = asset.text;
-            foreach (var item in (Dictionary<string, object>)Json.Deserialize(data))
-            {
-                parsedData.Add(item.Key, (string)item.Value);
-            }     
+        
+        foreach (var file in allFiles){
+            var asset = Resources.Load<TextAsset>(file.Replace(".json", ""));
+            
+            var data = asset.text;
+            
+            var parsedData = (Dictionary<string, object>)Json.Deserialize(data);
+            var split   = file.Split('/');
+            
+            SetTexts(parsedData, split[split.Length-1].Replace(".json",""), split[split.Length - 2]);
         }
-
-        texts.Add(language, parsedData);
     }
 
+    private void SetTexts(Dictionary<string, object> fileContent, string fileName, string language) {
+        var lang = LanguageMapper.Map(language.ToUpper());
 
-    public string GetText(string key)
-    {
-        if (!texts[language].ContainsKey(key))
-        {
-            Debug.LogError("Key " + key + " for language " + language + "not found");
+        foreach (var item in fileContent) {
+            if (!texts.ContainsKey(lang)) texts.Add(lang, new Dictionary<string, string>());
+            
+            texts[lang].Add($"{fileName}/{item.Key}", item.Value.ToString());
+            Debug.Log($"{lang} --- {fileName}/{item.Key} --- {item.Value}");
+        }
+    }
+
+    public string GetText(string key){
+        if (!texts[language].ContainsKey(key)){
+            Debug.LogError($"Key '{key}' for language '{language}' not found");
             return key;
         }
 
         return texts[language][key];
     }
-
-    public void SetTexts()
-    {
-        foreach (var item in textsInScene)
-        {
-            if (texts[language].ContainsKey(item.name))
-            {
-                item.text = texts[language][item.name];
-            }
-        }
-    }
 }
-
